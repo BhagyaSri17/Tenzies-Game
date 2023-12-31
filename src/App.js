5dc2374f123fb20e708f0aa301a3c76df0e8d529
BhagyaSri17 <bhagyasri1783@gmail.com>
2023-12-31 14:50:05 +0530

    Revert "Stashing local changes before revert"
    This reverts commit b187609072a38809248446ae23dea13e37a16cce.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React from "react";
-import Header from "./DigitalCard/DigitalCardHeader.js";
-import Content from "./DigitalCard/DigitalCardContent.js";
-import "./DigitalCard/DigitalCard.css";
+import Header from "./memeGenerator/MemeHeader.js";
+import Content from "./memeGenerator/memecontent.js";
+import "./memeGenerator/Meme.css";
 const App=()=>{
    return (
-    <div className="Main">
-      <div className="MainContent">
+    <div className="main">
     <Header/>
      <Content/>
-   </div>
    </div>
    )
 }
